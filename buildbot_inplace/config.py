2194eff725b5be764af6ae24afa2f1ecedfea24e
Jonathan Sieber <jonathan.sieber@hicknhack-software.com>
2018-06-26 14:46:49 +0200

    Setup GitPoller in per-project git-specific function

--- buildbot_inplace/config.py
+++ buildbot_inplace/config.py
@@ -150,51 +150,43 @@ class Wrapper(dict):
         allow_rules.append(AnyEndpointMatcher(role='nobody'))
         self['www']['authz'] = Authz(allowRules=allow_rules, roleMatchers=role_matcher)
 
-    def setup_git_triggers(self):
-        # Allow triggering through HTTP API at /change_hook
-        self['www']['change_hook_dialects'] = {'base': True}
+    def setup_git_poller(self, project):
+        workdir = './gitpoller-workdir/' + project.name
+        self.change_source.append(changes.GitPoller(
+            repourl=project.repo_url,
+            branches=[project.repo_branch],
+            pollAtLaunch=True,
+            project=project.name,
+            workdir=workdir
+        ))
 
-        for project in self.projects:
-            # Also regularly poll the repository (e.g. in case buildhost was shut down)
-            
-            workdir = './gitpoller-workdir/' + project.name
-            self.change_source.append(changes.GitPoller(
-                repourl=project.repo_url,
-                branches=[project.repo_branch],
-                pollAtLaunch=True,
-                project=project.name,
-                workdir=workdir
-            ))
+        # Manually add and configure git credential store
+        from subprocess import Popen
+        from os.path import expanduser
 
-            # Manually add and configure git credential store
-            from subprocess import Popen
-            from os.path import expanduser
+        git_cred_path = expanduser("~/.git-credentials")
+        Popen(["git", "init", "--bare", workdir])
+        try:
+            credlines = set(open(git_cred_path).readlines())
+        except IOError:
+            credlines = set()
 
-            git_cred_path = expanduser("~/.git-credentials")
-            Popen(["git", "init", "--bare", workdir])
-            try:
-                credlines = set(open(git_cred_path).readlines())
-            except IOError:
-                credlines = set()
+        # Prepare the GitPoller workdir with credentials
+        for cred in project.repo_credentials:
+            Popen(["git", "config", "--global", "credential.helper", "store"], cwd=workdir)
 
-            # Prepare the GitPoller workdir with credentials
-            for cred in project.repo_credentials:    
-                Popen(["git", "config","credential. " + cred.url + "username", cred.user], cwd=workdir)
-                Popen(["git", "config","credential.helper", "store"], cwd=workdir)
+            scheme, uri = cred.url.split("//")
+            credlines.add(scheme + "//" + cred.user + ":" + cred.password + "@" + uri + '\n')
 
-                scheme, uri = cred.url.split("//")
-                credlines.add(scheme + "//" + cred.user + ":" + cred.password + "@" + uri + '\n')
+        open(git_cred_path, "w").writelines(credlines)
 
-            open(git_cred_path, "w").writelines(credlines)
+        # Register a scheduler that reacts to changes on the repository
+        git_scheduler = SingleBranchScheduler(
+            name="Git-%s" % project.name,
+            builderNames=[project.name],
+            change_filter=ChangeFilter(branch=project.repo_branch))
 
-            # Register a scheduler that reacts to changes on the repository
-            git_scheduler = SingleBranchScheduler(
-                name="Git-%s" % project.name,
-                builderNames=[project.name],
-                change_filter=ChangeFilter(branch=project.repo_branch))
-
-            self.schedulers.named_set(git_scheduler)
-      
+        self.schedulers.named_set(git_scheduler)
 
     def setup_inplace(self):
         self.builders.clear()
@@ -238,4 +230,12 @@ class Wrapper(dict):
             self.builders.named_set(inplace_builder_config)
             self.schedulers.named_set(inplace_scheduler)
 
-        self.setup_git_triggers()
+        # Allow triggering through HTTP API at /change_hook
+        self['www']['change_hook_dialects'] = {'base': True}
+
+        # Also poll the repository (e.g. in case buildhost was shut down)
+        for project in self.projects:
+            if project.repo_type == 'git':
+                self.setup_git_poller(project)
+            else:
+                raise "SVN Repository Polling not implemented!"
